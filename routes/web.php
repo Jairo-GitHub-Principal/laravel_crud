76eeed667ef21bfd669ad7661c20f85626940038
jairocesar <jc200507@gmail.com>
2023-11-08 21:54:29 -0300

    primeiro commit, Erro de rota

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\BookController;
+ 
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// rota através de um função
+
+/*Route::get('/', function  () {
+    return view('index');
+});*/
+
+// rota direta através de um controller
+Route::get('/','BookController@index');
